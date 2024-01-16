e12b7fd8dfb4ce31f4e6ee3ecfe98d9187aea7e3
chrisportelli <portellichristopher@gmail.com>
2024-01-16 18:29:31 +0100

    Add files via upload

---
+++ BlazorRadioButtonGroupReproduction.Net6/Pages/CustomModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorRadioButtonGroupReproduction.Net6.Pages;
+
+public class CustomModel
+{
+    public bool FirstOption { get; set; }
+    public bool SecondOption { get; set; }
+}
---
+++ BlazorRadioButtonGroupReproduction.Net8/Components/CustomModel.cs
@@ -0,0 +1,7 @@
+﻿namespace BlazorRadioButtonGroupReproduction.Net8.Components;
+
+public class CustomModel
+{
+    public bool FirstOption { get; set; }
+    public bool SecondOption { get; set; }
+}
